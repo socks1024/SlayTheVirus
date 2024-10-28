2a66ead8b9d5fcbaa2fb942b452a0eb47df6f8e9
socks1024 <104053984+socks1024@users.noreply.github.com>
2024-10-28 12:02:57 +0800

    Delete intentions directory

--- intentions/attack_intention.gd
+++
@@ -1,9 +0,0 @@
-class_name AttackIntention
-extends BaseIntention
-
-func _init(amount:int,target:BaseCreature,source:BasePart):
-	super(IntentionType.ATTACK,amount,TargetType.PLAYER,target,source)
-
-
-func act():
-	battle_manager.use(AttackAction.new(target,source,amount))
--- intentions/defense_intention.gd
+++
@@ -1,9 +0,0 @@
-class_name DefenseIntention
-extends BaseIntention
-
-func _init(amount:int,target:BaseCreature,source:BasePart):
-	super(IntentionType.DEFENSE,amount,TargetType.BOSS,target,source)
-
-
-func act():
-	battle_manager.use(DefenseAction.new(target,source,amount))
