f3c888b6021e2809e6746781e015ae754d6670f5
socks1024 <104053984+socks1024@users.noreply.github.com>
2024-10-28 12:01:51 +0800

    Delete actions directory

--- actions/negate_trash_action.gd
+++
@@ -1,9 +0,0 @@
-class_name NegateTrashAction
-extends BaseAction
-
-func _init(target:BaseCreature,source:BaseCreature):
-	super(target,source)
-
-
-func act():
-	main.battle_manager.player_card_manager.negate_trash = true
